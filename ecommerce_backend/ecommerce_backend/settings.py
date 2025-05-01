59a59ba751dfd5593aa68b9fe631145af1919345
Adrian <akakiplimo@gmail.com>
2025-05-01 12:14:17 +0300

    feat: add requirements, settings and setup models

--- ecommerce_backend/ecommerce_backend/settings.py
+++ ecommerce_backend/ecommerce_backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'rest_framework_simplejwt',
+    'django_filters',
 
     # Local apps
     'products',
@@ -147,7 +148,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
